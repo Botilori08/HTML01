9a4d21ca774fd800e570bb528fc407d9b6e9949f
Boti Lóránt Ferenc <botil22@ady-nagyatad.sulinet.hu>
2024-05-22 08:46:30 +0200

    05.22

---
+++ Gyakorlás 2024/05.22/k_infoismfor_22maj_fl/Informatikai_ism_forrasok_K2121/3. Weboldalak kódolása és adatbázis-kezelés/adb-forras/megoldasok.sql
@@ -0,0 +1,19 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+-- 9. feladat:
+
+
+-- 11. feladat:
+
+
+-- 12. feladat:
+
+
+-- 13. feladat:
+
+
+-- 14. feladat:
+
+ 
+-- 15. feladat:
+
